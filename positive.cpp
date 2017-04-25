08395c442f7414f20b21d30a60b83a46fd907388
Pradeestark <Pradeestarck@gmail.com>
2017-04-25 20:26:47 +0530

    Create positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,13 @@
+}#include<iostream.h>
+void main()
+{
+int n;
+cout<<"enter the number";
+cin>>n;
+if(n>=0)
+{
+cout<<"the entered number is positive";
+}
+else
+cout<<"the entered number is negative";
+}
